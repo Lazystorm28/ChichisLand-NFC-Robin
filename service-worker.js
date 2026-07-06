d390902aa4f6fb4f625e09c89628f2fd32deb275
Lazystorm28 <cyoundo@gmail.com>
2026-07-06 23:33:05 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -10,41 +10,49 @@ const urlsToCache = [
 "/b1.html",
 "/c1.html",
 "/d1.html",
+"/e1.html",
 
 "/a2.html",
 "/b2.html",
 "/c2.html",
 "/d2.html",
+"/e2.html",
 
 "/a3.html",
 "/b3.html",
 "/c3.html",
 "/d3.html",
+"/e3.html",
 
 "/a4.html",
 "/b4.html",
 "/c4.html",
 "/d4.html",
+"/e4.html",
 
 "/images/a1.png",
 "/images/b1.png",
 "/images/c1.png",
 "/images/d1.png",
+"/images/e1.png",
 
 "/images/a2.png",
 "/images/b2.png",
 "/images/c2.png",
 "/images/d2.png",
+"/images/e2.png",
 
 "/images/a3.png",
 "/images/b3.png",
 "/images/c3.png",
 "/images/d3.png",
+"/images/e3.png",
 
 "/images/a4.png",
 "/images/b4.png",
 "/images/c4.png",
-"/images/d4.png"
+"/images/d4.png",
+"/images/e4.png"
 ];
 
 self.addEventListener("install", event => {
